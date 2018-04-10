c8e4ed0d4ef160e4304b07779b21f6e2cd060b2c
linhao <linhao@yahoo.com>
2018-04-10 00:06:37 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='haohello',
+       version='0.1',
+       description='This module greets the user.  ',
+       url='',                # usually a github URL
+       author='Hao Lin',
+       author_email='linhao@yahoo.com',
+       license='MIT',
+       packages=['haohello'],
+       install_requires=[ ],
+       zip_safe=False )
